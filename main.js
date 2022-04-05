9585f30c9ff557d5078eaca75516050524bdae58
JOB MONARI <88348681+narie-monarie@users.noreply.github.com>
2022-04-05 14:33:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,9 @@ const firebaseConfig = {
 
 const login = document.getElementById("login");
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
+
 login.addEventListener('click', (e) =>{
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
